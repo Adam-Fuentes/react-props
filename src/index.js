212c1703691ddcc8ab95abc8de41dc0b1d2f6dc4
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-10 09:15:01 +0100

    add props

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Card(){
+function Card(props){
   return(<div>
-    <h2>Beyonce</h2>
+    <h2>{props.name}</h2>
     <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      src={props.img}
       alt="avatar_img"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
+    <p>{props.tlf}</p>
+    <p>{props.mail}</p>
   </div>)
 }
 
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>    
+    <Card 
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
+      tlf="+123 456 789"
+      mail="b@beyonce.com"
+      />
+    <Card
+      name="Iron Man"
+      img="https://avatarfiles.alphacoders.com/139/thumb-139523.jpg"
+      tlf="+987 654 321"
+      mail="imtonystark@vengadores.com"
+    />
   </div>,
   document.getElementById("root")
 );
