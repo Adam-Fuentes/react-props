b823b98f3ff54e940fe00e162318041ea5f11e02
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-10 10:33:22 +0100

    add avatar to app
    pruebas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://www.onlinecasinoarchives.com/wp-content/uploads/Blizzard-Release-World-of-Warcraft-Cinematic.jpg" />
       <Card 
           name="Iron Man"
           img="https://avatarfiles.alphacoders.com/139/thumb-139523.jpg"
