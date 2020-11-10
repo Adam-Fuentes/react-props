ff5628d422f6806cec5171111014f6e2036089a0
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-10 10:30:12 +0100

    add detail component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
 import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 function Card(props){
     return(
         <div className="card">
             <div className="top">
             <h2 className="name">{props.name}</h2>
-            <Avatar img={props.img}/>
+            <Avatar img={props.img} />
             </div>
             <div className="bottom">
-            <p className="info">{props.tel}</p>
-            <p className="info">{props.email}</p>
+            <Detail 
+                detailInfo={props.tel} 
+            />
+            <Detail 
+                detailInfo={props.email} 
+            />
             </div>
       </div>
     )
